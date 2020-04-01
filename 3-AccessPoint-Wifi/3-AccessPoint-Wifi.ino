e94da9a46c9021f64e78b1c7d227931c1df165b1
pjqdyd <1909025079@qq.com>
2020-04-01 19:57:45 +0800

    init

---
+++ 3-AccessPoint-Wifi/3-AccessPoint-Wifi.ino
@@ -0,0 +1,41 @@
+/*
+NodeMCU接入点模式
+By 太极创客（http://www.taichi-maker.com）
+2019-03-11
+ 
+此程序用于演示如何将NodeMCU以接入点模式工作。通过此程序，您可以使用
+电脑或者手机连接NodeMCU所建立WiFi网络。
+ 
+网络名: ESP8266-NodeMCU-PJQDYD
+密码：12345678
+ 
+如需获得更多关于如何使用NodeMCU开发物联网的教程和资料信息
+请参考太极创客网站（http://www.taichi-maker.com）
+并在首页搜索栏中搜索关键字：物联网
+*/
+ 
+#include <ESP8266WiFi.h>        // 本程序使用ESP8266WiFi库
+ 
+const char *ssid = "ESP8266-NodeMCU-PJQDYD"; // 这里定义将要建立的WiFi名称
+                                   // 您可以将自己想要建立的WiFi名称填写入此处的双引号中
+ 
+const char *password = "12345678";  // 这里定义将要建立的WiFi密码。此处以12345678为示例
+                                    // 您可以将自己想要使用的WiFi密码放入引号内
+                                    // 如果建立的WiFi不要密码，则在双引号内不要填入任何信息
+ 
+void setup() {
+  Serial.begin(9600);              // 启动串口通讯
+ 
+  WiFi.softAP(ssid, password);     // 此语句是重点。WiFi.softAP用于启动NodeMCU的AP模式。
+                                   // 括号中有两个参数，ssid是WiFi名。password是WiFi密码。
+                                   // 这两个参数具体内容在setup函数之前的位置进行定义。
+ 
+  
+  Serial.print("Access Point: ");    // 通过串口监视器输出信息
+  Serial.println(ssid);              // 告知用户NodeMCU所建立的WiFi名
+  Serial.print("IP address: ");      // 以及NodeMCU的IP地址
+  Serial.println(WiFi.softAPIP());   // 通过调用WiFi.softAPIP()可以得到NodeMCU的IP地址
+}
+ 
+void loop() { 
+}
